68901e740aa6dfc3b277d94f9b0187f6ce897850
KrushankPatel <200020116052ait@gmail.com>
2023-02-26 14:53:52 +0530

    first commit

---
+++ profile.js
@@ -0,0 +1,38 @@
+// <----------Text Animation-------------->
+
+const text = document.querySelector(".span-2");
+
+const textLoad = () => {
+  setTimeout(() => {
+    text.textContent = "Web Developer.";
+  }, 0);
+
+  setTimeout(() => {
+    text.textContent = "Intermideate Coder.";
+  }, 4000);
+
+  setTimeout(() => {
+    text.textContent = "Student.";
+  }, 8000);
+};
+
+textLoad();
+setInterval(textLoad, 12000);
+
+// <-----------------tabs---------------------------->
+
+let myResume = (evt, resume) => {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(resume).style.display = "";
+  evt.currentTarget.className += " active";
+};
+
+document.getElementById("defaultOpen").click();
